92097f1f59e47f13840eff4a8a0d424a4a8c3c37
eluwasiema <kacperkupiec04@gmail.com>
2022-11-01 15:49:02 +0100

    Calc Update 2

--- kalkulator/script.js
+++ kalkulator/script.js
@@ -11,8 +11,8 @@ function onload(){
 
     }
 }
-var kolejny = '';
-var index = 0;
+var kolejny = '';   // wynik
+var index = 0;      // index-1 to 
 var id = 0;         // obecne id
 let lastid = '';   // poprzednie id
 var allid = [];    // wszystkie id w array
@@ -25,43 +25,40 @@ function zczytanieid(ids){
 
 function przycisniecie(wartosc){        // wartosc to wcisniety obecnie przycisk
     if(wartosc != "=" && wartosc != "c"){
-        var aktualna = wartosc;
-        console.log(aktualna);
-        var kolejny = document.getElementById("numer").value += wartosc;
-		var poprawa = document.getElementById("numer").value;
-        var ostatnieobl = kolejny.length - 2;
-		var krok = poprawa[ostatnieobl];
-		console.log(krok);
-        console.log(kolejny);
-        console.log(allid);
-        console.log(lastid);
-        console.log(id);
-	if(lastid == "specjalny" && lastid == id){
-        alert("Blad przy wprowadzaniu liczby");
-        document.getElementById("numer").value = "";
-        allid = [];
-        id = 0;
+            var kolejny = document.getElementById("numer").value += wartosc;
+		    var poprawa = document.getElementById("numer").value;
+            var ostatnieobl = kolejny.length - 2;
+		    var krok = poprawa[ostatnieobl];
+	    if(lastid == "specjalny" && lastid == id){
+            alert("Blad przy wprowadzaniu liczby");
+            document.getElementById("numer").value = "";
+            allid = [];
+            id = 0;
+            index = 0;
+        }
+        if(id == "specjalny" && krok == undefined && wartosc != "-"){
+            alert("Blad przy wprowadzaniu liczby");
+            document.getElementById("numer").value = "";
+            allid = [];
+            id = 0;
+            index = 0;
+        }
     }
-    if(id == "specjalny" && krok == undefined && wartosc != "-"){
-        alert("Blad przy wprowadzaniu liczby");
-        document.getElementById("numer").value = "";
-        allid = [];
-        id = 0;
-    }
-    }
-    else if(wartosc == "="){            // dziala
+    else if(wartosc == "="){            // dziala oprocz nawiasow, nie lapie bledu
         var sum = document.getElementById("numer").value;
         try{
+            var evalsum = eval(sum);
             console.log(eval(sum));
-        } catch (e) {
-            if(e instanceof SyntaxError){
-                alert("Blad przy wprowadzaniu liczby");
-                document.getElementById("numer").value = "";
-            }
-            else{
-                document.getElementById("numer").value = eval(sum);
+            document.getElementById("numer").value = evalsum;
+            if(document.getElementById("numer").value == ""){
                 allid = [];
                 id = 0;
+                index = 0;
+            }
+        } catch (e) {
+            if(e instanceof EvalError){
+                alert("Blad przy wprowadzaniu liczby");
+                document.getElementById("numer").value = "";
             }
         }
     }
@@ -69,5 +66,6 @@ function przycisniecie(wartosc){        // wartosc to wcisniety obecnie przycisk
         document.getElementById("numer").value = "";
         allid = [];
         id = 0;
+        index = 0;
     }
 }
